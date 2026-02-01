9f6dc0287fe6f8589826ca4194f3abb99ddf08b4
mai-antar <maie.antar.hashem@gmail.com>
2026-02-01 10:13:05 +0200

    mai

--- TrainigSectorDataEntry/ViewModel/HistoryBreifVM.cs
+++ TrainigSectorDataEntry/ViewModel/HistoryBreifVM.cs
@@ -12,32 +12,32 @@ namespace TrainigSectorDataEntry.ViewModel
 
 
         [Required(ErrorMessage = ".برجاء ادخال الاسم باللغة العربية")]
-        [RegularExpression(@"^[\u0600-\u06FF\s]+$", ErrorMessage = ".يجب كتابة لغة عربية فقط")]
+        //[RegularExpression(@"^[\u0600-\u06FF\s]+$", ErrorMessage = ".يجب كتابة لغة عربية فقط")]
         public string NameAr { get; set; } = null!;
 
 
         [Required(ErrorMessage = ".برجاء ادخال الاسم باللغة الانجليزي")]
-        [RegularExpression(@"^[a-zA-Z0-9\s.,'-]+$", ErrorMessage = ".يجب كتابة لغة انجليزية فقط")]
+        //[RegularExpression(@"^[a-zA-Z0-9\s.,'-]+$", ErrorMessage = ".يجب كتابة لغة انجليزية فقط")]
         public string NameEn { get; set; } = null!;
 
 
         [Required(ErrorMessage = ".برجاء ادخال عنوان النبذة التاريخية باللغة العربية")]
-        [RegularExpression(@"^[\u0600-\u06FF\s]+$", ErrorMessage = ".يجب كتابة لغة عربية فقط")]
+        //[RegularExpression(@"^[\u0600-\u06FF\s]+$", ErrorMessage = ".يجب كتابة لغة عربية فقط")]
         public string? TitleAr { get; set; }
 
 
         [Required(ErrorMessage = ".برجاء ادخال عنوان النبذة التاريخية باللغة الانجليزية")]
-        [RegularExpression(@"^[a-zA-Z0-9\s.,'-]+$", ErrorMessage = ".يجب كتابة لغة انجليزية فقط")]
+        //[RegularExpression(@"^[a-zA-Z0-9\s.,'-]+$", ErrorMessage = ".يجب كتابة لغة انجليزية فقط")]
         public string? TitleEn { get; set; }
 
 
         [Required(ErrorMessage = ".برجاء ادخال تفاصيل النبذة التاريخية باللغة العربية")]
-        [RegularExpression(@"^[\u0600-\u06FF\s]+$", ErrorMessage = ".يجب كتابة لغة عربية فقط")]
+        //[RegularExpression(@"^[\u0600-\u06FF\s]+$", ErrorMessage = ".يجب كتابة لغة عربية فقط")]
         public string? DescriptionAr { get; set; }
 
 
         [Required(ErrorMessage = ".برجاء ادخال تفاصيل النبذة التاريخية باللغة الانجليزية")]
-        [RegularExpression(@"^[a-zA-Z0-9\s.,'-]+$", ErrorMessage = ".يجب كتابة لغة انجليزية فقط")]
+        //[RegularExpression(@"^[a-zA-Z0-9\s.,'-]+$", ErrorMessage = ".يجب كتابة لغة انجليزية فقط")]
         public string? DescriptionEn { get; set; }
 
         public bool IsActive { get; set; }
